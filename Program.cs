d22a198c4c24ce8cf42117742a06f0558c88c512
Ecem Doga <96545968+dogaklnc@users.noreply.github.com>
2023-01-16 16:13:11 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,11 +1,23 @@
 ﻿using System;
-namespace console_programlama;
-
-class Program {
-    public static void Main(string[] args)
+ namespace hata_yonetimi;
+ {
+    class Program
     {
-        Console.WriteLine("Hello Word!");
-        Console.WriteLine("Hi!");
-        Console.ReadLine();
+        static void Main(string[] args)
+        {
+            Console.Writeline("Bir sayı giriniz");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            Console.Writeline("Girişiniz Olduğu Sayı;"+ sayi);
+            {
+                catch(Exception ex)
+                {
+                    Console.Writeline("Hata"+ ex.Message);
+                }
+                finally
+                {
+                    Console.Writeline("işlem tamam");
+                }
+            }
+        }
     }
-}
+ }
